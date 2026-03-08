4982247ed877078506125acba5c4adb1e00c6bbf
Yash Grover <ygrover178@gmail.com>
2026-03-08 21:58:02 +0530

    bug fixes and theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 import Navbar from "@/src/components/navbar/navbar";
-import Footer from "../src/components/footer/footer";
+import Footer from "@/src/components/footer/footer";
 import ExpertOpinionCTA from "@/src/components/ExpertOpinionCTA";
 
 export default function Home() {
   return (
-    <body>
+    <>
       <Navbar/>
       <ExpertOpinionCTA/>
       <Footer />
-    </body>
+    </>
   )
 }
